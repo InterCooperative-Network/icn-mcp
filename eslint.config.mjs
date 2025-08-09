d90e4d454e97ba7f30ff4128cfeb7aeef6a70fd0
Matt Faherty <matt.faherty@gmail.com>
2025-08-09 05:57:24 +0000

    chore(root): run all workspaces for build/test; add engines and ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {}
+  }
+];
+
